28db6f900e99497c3f6359952357cbf7cc78dc06
unknown <kennethdeanoliver@gmail.com>
2024-02-22 19:11:44 +0700

    movement

--- src/HelloWorld.js
+++ src/HelloWorld.js
@@ -9,6 +9,9 @@ export default class HelloWorldScene extends Phaser.Scene
         this.platforms = undefined
 
         this.player = undefined
+        this.companion = undefined
+
+        this.cursors = undefined
     }
     preload(){
         this.load.image('background', 'images/background (placeholder).png')
@@ -21,12 +24,36 @@ export default class HelloWorldScene extends Phaser.Scene
 
         this.platforms = this.physics.add.staticGroup()
         this.platforms.create(400, 568, 'platform').setScale(2).refreshBody()
-        this.physics.add.collider(this.player,this.platforms)
 
         this.player = this.physics.add.sprite(100, 468, 'Tulip').setScale(0.25)
-        this.physics.add.sprite(200, 502, 'Atticus').setScale(0.25)
+        this.player.setCollideWorldBounds(true)
+        this.physics.add.collider(this.player, this.platforms)
+        this.anims.create({
+            key: 'running away',
+            frames: this.anims.generateFrameNumbers('Tulip', {start: 1, end: 4}),
+            frameRate: 3,
+            repeat: -1
+        })
+
+        this.companion = this.physics.add.sprite(200, 502, 'Atticus').setScale(0.25)
+        this.physics.add.collider(this.companion, this.platforms)
+        this.physics.add.overlap(this.player, this.companion)
+        this.anims.create({
+            key: 'a tourginia of corginia',
+            frames: this.anims.generateFrameNumbers('Atticus', {start: 1, end: 4}),
+            frameRate: 3,
+            repeat: -1
+        })
+
+        this.cursors = this.input.keyboard.createCursorKeys()
     }
     update(){
-
+        if (this.cursors.right.isDown){
+            this.player.setVelocityX(30)
+            this.player.anims.play('running away', true)
+        }
+        if (this.cursors.up.isDown){
+            this.player.setVelocityY(-40)
+        }
     }
 }
