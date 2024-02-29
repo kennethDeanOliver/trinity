989cba2bfa32d4b9cda75313a3e654cfdd761f24
unknown <kennethdeanoliver@gmail.com>
2024-02-29 19:55:10 +0700

    jumping

--- src/HelloWorld.js
+++ src/HelloWorld.js
@@ -7,17 +7,19 @@ export default class HelloWorldScene extends Phaser.Scene
     }
     init(){
         this.platforms = undefined
-
         this.player = undefined
         this.companion = undefined
+        this.obstacles = undefined
 
-        this.cursors = undefined
+        this.cursors = undefined   
+        this.grounded = true
     }
     preload(){
         this.load.image('background', 'images/background (placeholder).png')
         this.load.image('platform', 'images/platform (placeholder).png')
-        this.load.spritesheet('Tulip', 'images/tulip & one-one.png', {frameWidth: 405, frameHeight: 570})
-        this.load.spritesheet('Atticus', 'images/atticus.png', {frameWidth: 360, frameHeight: 300})
+        this.load.image('obstacle', 'images/rock.png')
+        this.load.spritesheet('Tulip', 'images/tulip & one-one.png', {frameWidth: 405, frameHeight: 540})
+        this.load.spritesheet('Atticus', 'images/atticus.png', {frameWidth: 360, frameHeight: 270})
     }
     create(){
         this.add.image(400, 300, 'background')
@@ -36,24 +38,36 @@ export default class HelloWorldScene extends Phaser.Scene
         })
 
         this.companion = this.physics.add.sprite(200, 502, 'Atticus').setScale(0.25)
+        this.companion.setCollideWorldBounds(true)
         this.physics.add.collider(this.companion, this.platforms)
         this.physics.add.overlap(this.player, this.companion)
         this.anims.create({
             key: 'a tourginia of corginia',
             frames: this.anims.generateFrameNumbers('Atticus', {start: 1, end: 4}),
-            frameRate: 3,
+            frameRate: 5,
             repeat: -1
+        })
+
+        this.obstacles = this.physics.add.group({
+            key: 'rocks',
+            repeat: 5,
+            setXY: {x: 50, y: 0, stepX: 70}
         })
 
         this.cursors = this.input.keyboard.createCursorKeys()
     }
     update(){
-        if (this.cursors.right.isDown){
-            this.player.setVelocityX(30)
-            this.player.anims.play('running away', true)
+        this.player.setVelocityX(50)
+        this.player.anims.play('running away', true)
+        this.companion.setVelocityX(80)
+        this.companion.anims.play('a tourginia of corginia', true)
+        if (this.cursors.up.isDown && this.grounded){
+            this.player.setVelocityY(-200)
+            this.grounded = false
+            this.time.delayedCall(2000, this.endJump, [], this)
         }
-        if (this.cursors.up.isDown){
-            this.player.setVelocityY(-40)
-        }
+    }
+    endJump(){
+        this.grounded = true
     }
 }
